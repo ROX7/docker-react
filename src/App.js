0083a0592cec0e20c024b9dd8ecea980f9deb519
YanisBled <yan.bled@gmail.com>
2022-12-27 20:34:52 +0100

    add travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ddddd
+          dddddss
         </p>
         <a
           className="App-link"
